70451ca957d80f7fd1c1d93889167932855fc10f
Blake Guilloud <blake.guilloud@gmail.com>
2017-10-25 22:04:13 -0400

    Inital build w/ just my front end project.

---
+++ index.js
@@ -0,0 +1,34 @@
+const chalk = require('chalk');
+const emoji = require('node-emoji');
+const download = require('download-git-repo');
+
+const log = console.log;
+const beers = emoji.get('beers');
+const noEntry = emoji.get('no_entry');
+const helmet = emoji.get('helmet_with_white_cross');
+
+const repos = {
+  front: 'BlakeGuilloud/webpack-babel-react',
+};
+
+const logError = () =>
+  log(chalk.red(`Error ${noEntry}`));
+
+const logSuccess = dir =>
+  log(chalk.green(`${beers} ${beers} Repo '${dir}' has been created`));
+
+const logProgress = () =>
+  log(chalk.cyan(`${helmet} Initialized...`));
+
+const handleResponse = (err, dir) =>
+  err ? logError() : logSuccess(dir);
+
+const create = (type, dir) =>
+  download(repos[type], dir, (err) => handleResponse(err, dir));
+
+module.exports = {
+  create,
+  logProgress,
+};
+
+
---
+++ program.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const { create, logProgress } = require('./index');
+
+program
+  .version('0.1.0')
+  .command('create <type> <dir>')
+  .action(logProgress)
+  .action(create);
+
+program.parse(process.argv);
