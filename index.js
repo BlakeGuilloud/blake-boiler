5a14a2d6f7c731de8a5f3ce2148dac9fb1b65d11
Blake Guilloud <blake.guilloud@gmail.com>
2017-11-11 23:10:18 -0500

    updated front + react paths.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const noEntry = emoji.get('no_entry');
 const helmet = emoji.get('helmet_with_white_cross');
 
 const repos = {
-  front: 'BlakeGuilloud/webpack-babel-react',
+  react: 'BlakeGuilloud/webpack-babel-react',
+  front: 'BlakeGuilloud/webpack-babel-front',
 };
 
 const logError = err =>
